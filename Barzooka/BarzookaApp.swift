5f7dc635d96f6f3adfbdbec599e0f019aa23c5c8
Zhuo Feng <zhuo.feng@shopee.com>
2022-06-21 12:09:00 +0800

    Initial Commit

---
+++ Barzooka/BarzookaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarzookaApp.swift
+//  Barzooka
+//
+//  Created by Zhuo FENG on 2022/6/21.
+//
+
+import SwiftUI
+
+@main
+struct BarzookaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Barzooka/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Barzooka
+//
+//  Created by Zhuo FENG on 2022/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
